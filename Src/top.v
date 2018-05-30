acef8cf68a9901d27b8dd5e9d31f5026958a1376
joshstern1 <jstern19@bu.edu>
2018-05-30 17:29:55 -0400

    Update top.v

--- Src/top.v
+++ Src/top.v
@@ -19,27 +19,7 @@ inside the fifo
 //extra bit/counting down is 1 if the wait count was ever set to the proper latency
 /////////////////////////////////////////////////////////////////////////////////*/
 
-//dont need rank, root
-//combine algtype and op
-//make reduction bit apart of op/algtype
-//ranem commsize context_id
-module top(dataIn, clk, rst, Outpacket, src_x, src_y, src_z, dst_x, dst_y, dst_z, op, contextId, algtype, tag, valid, done);
-input [31:0]dataIn;
-input clk;
-input rst;
-input [4:0] op;
-input [3:0] tag;
-input [2:0] src_x;
-input [2:0] src_y;
-input [2:0] src_z;
-input [2:0] dst_x;
-input [2:0] dst_y;
-input [2:0] dst_z;
-input [1:0] algtype;
-input [3:0] contextId;
-input valid;
-output done;
-output[63:0]Outpacket;
+module top(Outpacket, done, valid_out, clk, rst, valid_in, dst_z, dst_y, dst_x, src_z, src_y, src_x, contextId, tag, algtype, op, payload);
 
 parameter rank = 9'b0;
 parameter root = 9'b0;
@@ -52,40 +32,69 @@ parameter root_x = 3'b000;
 
 parameter Comm_world_size = 8;
 
-parameter ReductionTableWidth = 73;
+parameter FlitWidth = 73;
+parameter PayloadWidth=32;
+parameter opPos = 32;
+parameter opWidth = 4;
+parameter AlgTypePos = 36;
+parameter AlgTypeWidth = 2;
+parameter TagPos=38;
+parameter TagWidth = 8;
+parameter ContextIdPos = 46;
+parameter ContextIdWidth = 8;
+parameter Src_XPos = 54;
+parameter Src_YPos = 57;
+parameter Src_ZPos = 60;
+parameter Src_XWidth = 3;
+parameter Src_YWidth = 3;
+parameter Src_ZWidth = 3;
+parameter Dst_XPos = 63;
+parameter Dst_YPos = 66;
+parameter Dst_ZPos = 69;
+parameter Dst_XWidth = 3;
+parameter Dst_YWidth = 3;
+parameter Dst_ZWidth = 3;
+parameter SrcPos = 54;
+parameter SrcWidth = 9;
+parameter DstPos = 63;
+parameter DstWidth = 9;
+parameter ValidBitPos = 72;
+
+parameter ReductionTableWidth = 82;
 parameter ReductionTableSize = 2;
 parameter AdderLatency = 14;
 
-parameter DataWidth = 64;
-parameter PayloadLen=32;
-parameter opPos = 32;
-parameter opWidth = 5;
-parameter RankPos = 37;
-parameter RankWidth = 3;
-parameter IndexPos=46;
-parameter IndexWidth = 4;
-parameter PacketTypePos = 52;
-parameter PacketTypeWidth = 4;
-parameter DstPos = 56;
-parameter DstWidth = 3;
-parameter SrcPos = 59;
-parameter SrcWidth = 3;
-parameter ReductionBitPos=62;
-parameter ValidBitPos = 63;
-parameter ChildrenPos=64;
+parameter ReductionBitPos=35;
+
+parameter ChildrenPos=73;
 parameter ChildrenWidth=3;
-parameter WaitPos = 67;
+parameter WaitPos = 76;
 parameter WaitWidth = 4;
-parameter ExtraWaitPos=71;
-parameter LeafBitPos=72;
+parameter ExtraWaitPos=80;
+parameter LeafBitPos=81;
+
+input clk;
+input rst;
+input [PayloadWidth-1:0]payload;
+input [opWidth-1:0] op;
+input [AlgTypeWidth-1:0] algtype;
+input [TagWidth-1:0] tag;
+input [ContextIdWidth-1:0] contextId;
+input [Src_XWidth-1:0] src_x, src_y, src_z;
+input [Dst_XWidth-1:0] dst_x, dst_y, dst_z;
+input valid_in;
+output valid_out;
+output done;
+output[FlitWidth-1:0]Outpacket;
+
 
 //packeter
-wire [DataWidth-1:0]packeterOut;
+wire [FlitWidth-1:0]packeterOut;
 wire buf_empty;
 wire buf_full;
 
 //instr
-wire [DataWidth+ChildrenWidth-1:0]instr_out;
+wire [FlitWidth+ChildrenWidth-1:0]instr_out;
 
 //fifo
 wire [12:0]fifo_counter;
@@ -93,20 +102,19 @@ wire rd_en;
 wire wr_en;
 
 //reduction unit
-wire [DataWidth+ChildrenWidth-1:0]packetA;
-wire [IndexWidth-1:0]packetIndex;
+wire [FlitWidth+ChildrenWidth-1:0]packetA;
+wire [TagWidth-1:0]packetIndex;
 wire [ChildrenWidth-1:0]children_count;
 wire reductiontype;
 wire[ReductionTableWidth-1:0] reduction_table_entry;
 wire [WaitWidth-1:0]WaitCount;
-wire [PayloadLen-1:0] dataC;
-wire [PayloadLen-1:0] sum;
+wire [PayloadWidth-1:0] dataC;
+wire [PayloadWidth-1:0] sum;
 
 reg [8:0]counter;
 reg done;
 reg done_index;
 reg[ReductionTableWidth-1:0] reduction_table[ReductionTableSize-1:0];
-reg [IndexWidth-1:0] reduction_table_index;
 reg [ReductionTableSize-1:0]i;
 reg [ReductionTableSize-1:0]j;
 reg [ReductionTableSize-1:0]k;
@@ -117,19 +125,18 @@ assign wr_en = 1'b1;
 
 packeter P1 (
  .packeterOut(packeterOut),
- .dataIn(dataIn),
+ .payload(payload),
  .op(op),
- .commsize(contextId),
+ .algtype(algtype),
+ .tag(tag),
+ .contextId(contextId),
  .src_x(src_x),
  .src_y(src_y),
  .src_z(src_z),
  .dst_x(dst_x),
  .dst_y(dst_y),
  .dst_z(dst_z),
- .tag(tag),
- .algtype(algtype),
- .dst(dst),
- .valid(valid)
+ .valid(valid_in)
 );
  
 reduce_instr R1 (
@@ -152,48 +159,41 @@ fifo F1 (
  .fifo_counter(fifo_counter)
 );
 
-assign packetIndex = packetA[IndexPos+IndexWidth-1:IndexPos];
+assign packetIndex = packetA[TagPos+TagWidth-1:TagPos];
 assign children_count = packetA[ChildrenPos+ChildrenWidth-1:ChildrenPos];
 assign reductiontype = packetA[ReductionBitPos];
 assign WaitCount = reduction_table[packetIndex][WaitPos+WaitWidth-1:WaitPos];
 
-/*
-ip core that performs floating point single precision addition. dont forget to make this double precision if possible
-*/
+//ip core that performs floating point single precision addition. dont forget to make this double precision if possible
 addy A1(
-  .a(reduction_table[packetIndex][PayloadLen-1:0]),
-  .b(packetA[PayloadLen-1:0]),
+  .a(reduction_table[packetIndex][PayloadWidth-1:0]),
+  .b(packetA[PayloadWidth-1:0]),
   .clk(clk),
   .s(sum)
 );
 
-/*
-the following assignment statements are responsible for creating the output packet.
+/* the following assignment statements are responsible for creating the output packet.
 they combine the results of the reduction table such as the payload, destination, etc.
-dataC is just used for simulation and analysis.  
-*/
-assign reduction_table_entry= reduction_table[done_index];
+dataC is just used for simulation and analysis. */
+assign reduction_table_entry = reduction_table[done_index];
+assign Outpacket[ContextIdPos+ContextIdWidth-1:0] = reduction_table_entry[ContextIdPos+ContextIdWidth-1:0];
+assign Outpacket[SrcPos+SrcWidth-1:SrcPos] = rank;	//only if outgoing unit, if local unit, keep the src the same
+assign Outpacket[DstPos+DstWidth-1:DstPos] = reduction_table_entry[DstPos+DstWidth-1:DstPos];
 assign Outpacket[ValidBitPos] = 1;
-assign Outpacket[ReductionBitPos] = 1;
-assign Outpacket[DstPos+DstWidth-1:0] = reduction_table_entry[DstPos+DstWidth-1:0];
-assign Outpacket[SrcPos+SrcWidth-1:SrcPos] = rank;
-assign dataC = Outpacket[PayloadLen-1:0];
+assign dataC = Outpacket[PayloadWidth-1:0];
+assign valid_out = 1'b1;
 
-/*
-below is the logic for the reduction table.  every instruction is nonblocking and
-changes on the positive edge of the clock.
-*/
+/* below is the logic for the reduction table.  every instruction is nonblocking and
+changes on the positive edge of the clock. */
 
 
 always@(posedge clk) begin
-  /*
-  if the rst is set, set everything in the reduction table to 0.
+  /* if the rst is set, set everything in the reduction table to 0.
   the rst must always be set and then turned off before any reductions
-  can take place.
-  */
+  can take place. */
    if (rst)begin	
 		for(k=0;k<ReductionTableSize;k=k+1)begin
-			reduction_table[k]<=73'b0;
+			reduction_table[k]<=82'b0;
 		end		
 		counter<=0;
 		done<=0;
@@ -206,10 +206,8 @@ always@(posedge clk) begin
 			counter<=counter+1;
 		end     
   
-   /*
-  if the incoming packet is to be placed in a reduction table slot that is either empty or invalid,
-  the parameters of the incoming packet (packetA) are placed in its table slot.
-   */
+   /* if the incoming packet is to be placed in a reduction table slot that is either empty or invalid,
+  the parameters of the incoming packet (packetA) are placed in its table slot. */
   
 		if (reductiontype && (reduction_table[packetIndex][ValidBitPos] ==0 ))begin //if slot is empty
 			reduction_table[packetIndex][ChildrenPos+ChildrenWidth-1:ChildrenPos]<=children_count+1;
@@ -225,10 +223,8 @@ always@(posedge clk) begin
 			end
 		end
   
-  /*
-  if a reduction was just completed, then that reduction's corresponding reduction table slot must
-  be cleared. 
-  */
+  /* if a reduction was just completed, then that reduction's corresponding reduction table slot must
+  be cleared. */
 		if (done)begin
 			//reduction_table[done_index][ValidBitPos]<=0;
 			if (reductiontype)begin
@@ -254,7 +250,7 @@ always@(posedge clk) begin
    
 				//if incoming packet needs to be placed in a different slot than the reduction that was just completed
 				else if (done_index!=packetIndex)begin
-					reduction_table[done_index]<=73'b0; //clear out reduction table slot that just completed
+					reduction_table[done_index]<=82'b0; //clear out reduction table slot that just completed
      
      /*
      check to make sure that the incoming packet to the reduction table was not already received.  if the incoming
@@ -288,9 +284,6 @@ always@(posedge clk) begin
 		end
   
   
-		reduction_table_index<=packetIndex;
-  
-  
   /* the following loop checks all reduction table entry to see if their wait count has fallen to 0,
   meaning the reduction table entry's corresponding ip core output is ready to be copied into
   the reduction table.  A mux is used with the op input serving as the select variable.
@@ -302,7 +295,7 @@ always@(posedge clk) begin
    if ((reduction_table[i][WaitPos+WaitWidth-1:WaitPos]==0)&&(reduction_table[i][ValidBitPos]==1)&&(reduction_table[i][ExtraWaitPos]==1))begin
      if((reduction_table[i][LeafBitPos]==0)&&(counter>AdderLatency))begin
       case(reduction_table[i][opPos+opWidth-1:opPos])
-      5'b00000: reduction_table[i][PayloadLen-1:0] <= sum;
+      4'b1111: reduction_table[i][PayloadWidth-1:0] <= sum;
       endcase
      end
      reduction_table[i][ExtraWaitPos]<=0;
