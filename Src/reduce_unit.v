edd65335a74e70828b36ac169e39d656ac89f9db
joshstern1 <jstern19@bu.edu>
2018-06-25 14:36:27 -0400

    Update reduce_unit.v

--- Src/reduce_unit.v
+++ Src/reduce_unit.v
@@ -21,8 +21,104 @@ inside the fifo
 //extra bit/counting down is 1 if the wait count was ever set to the proper latency
 /////////////////////////////////////////////////////////////////////////////////*/
 
-`include "parameters.v"
 module reduce_unit(Outpacket, done, valid_out, clk, rst, packetA, rd_en, wr_en, fifo_counter, buf_empty, buf_full);
+
+//////////////////////////////////////////
+//current rank, root, and world size
+parameter cur_rank = 9'b0;
+parameter root = 9'b0;
+parameter rank_z = 3'b0;
+parameter rank_y = 3'b0;
+parameter rank_x = 3'b0;
+parameter root_z = 3'b0;
+parameter root_y = 3'b0;
+parameter root_x = 3'b0;
+
+parameter lg_numprocs = 3;
+parameter num_procs = 1 << lg_numprocs;
+
+///////////////////////////////////////////
+//packet structure
+parameter PayloadWidth=32;
+parameter opPos = PayloadWidth;
+parameter opWidth = 4;
+parameter AlgTypePos = opPos+opWidth;
+parameter AlgTypeWidth = 2;
+parameter TagPos=AlgTypePos+AlgTypeWidth;
+parameter TagWidth = 8;
+parameter ContextIdPos = TagPos+TagWidth;
+parameter ContextIdWidth = 8;
+parameter RankPos = ContextIdPos + ContextIdWidth;
+parameter RankWidth = 9;
+parameter Src_XPos = RankPos+RankWidth;
+parameter Src_XWidth = 3;
+parameter Src_YPos = Src_XPos+Src_XWidth;
+parameter Src_YWidth = 3;
+parameter Src_ZPos = Src_YPos+Src_YWidth;
+parameter Src_ZWidth = 3;
+parameter Dst_XPos = Src_ZPos+Src_ZWidth;
+parameter Dst_XWidth = 3;
+parameter Dst_YPos = Dst_XPos+Dst_XWidth;
+parameter Dst_YWidth = 3;
+parameter Dst_ZPos = Dst_YPos+Dst_YWidth;
+parameter Dst_ZWidth = 3;
+parameter SrcPos = Src_XPos;
+parameter SrcWidth = Src_XWidth+Src_YWidth+Src_ZWidth;
+parameter DstPos = Dst_XPos;
+parameter DstWidth = Dst_XWidth+Dst_YWidth+Dst_ZWidth;
+parameter ValidBitPos = Dst_ZPos+Dst_ZWidth;
+parameter FlitWidth = ValidBitPos + 1;
+
+
+/////////////////////////////////////////
+//children and countdown
+parameter ChildrenPos=ValidBitPos+1;
+parameter ChildrenWidth=lg_numprocs;
+parameter WaitPos = ChildrenPos+ChildrenWidth;
+parameter WaitWidth = 4;
+parameter ExtraWaitPos=WaitPos+WaitWidth;
+parameter LeafBitPos=ExtraWaitPos+1;
+
+ 
+//////////////////////////////////////////
+//reduce unit table and adder
+parameter ReductionTableWidth = LeafBitPos+1;
+parameter ReductionTableSize = 2;
+parameter AdderLatency = 14;
+parameter ReductionBitPos=opPos+opWidth-1;
+
+
+///////////////////////////////////////
+//fifo
+parameter fifo_lg_size = 12;
+parameter FifoSize = 1<<fifo_lg_size;
+
+/////////////////////////////////////
+//communicator table
+parameter CommTableWidth = (lg_numprocs+2)*DstWidth + lg_numprocs*2+2;
+parameter CommTableSize = 4;
+parameter lgCommSizePos = lg_numprocs*DstWidth;
+parameter CommChildrenPos = lgCommSizePos+lg_numprocs+1;
+parameter LocalRankPos = CommChildrenPos + lg_numprocs;
+parameter NewCommWidth = CommTableWidth+ContextIdWidth;
+
+////////////////////////////////////
+//algorithmic opcodes
+parameter Scan = 4'b0011;
+parameter AlltoAll = 4'b0100;
+parameter LargeBcast = 4'b0101;
+parameter MediumBcast = 4'b0110;
+parameter ShortBcast = 4'b0111;
+parameter Scatter = 4'b1000;
+parameter LargeAllGather = 4'b1001;
+parameter ShortAllGather = 4'b1010;
+parameter Gather = 4'b1011;
+parameter ShortReduce = 4'b1100;
+parameter LargeReduce = 4'b1101;
+parameter ShortAllReduce = 4'b1110;
+parameter LargeAllReduce = 4'b1111;
+
+////////////////////////////////////
 
 input clk, rst;
 input [FlitWidth+ChildrenWidth-1:0]packetA;
