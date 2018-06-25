3c0bd411bd01c633a3d3a8462e49a993f53db91e
joshstern1 <jstern19@bu.edu>
2018-06-25 14:36:44 -0400

    Update reduce_instr.v

--- Src/reduce_instr.v
+++ Src/reduce_instr.v
@@ -24,12 +24,116 @@ inside the fifo
 
 	*|	52  |51-43|	 42-34   | 33-31  |   30-27   |26-18| 17-9 | 8-0 |     	
 	*|valid|root |local_rank|children|lg_commsize|third|second|first|
+	
+	//incoming communicator packet 
+	
+	*|	60  |  59-52  |51-43|  42-34   | 33-31  |   30-27   |26-18| 17-9 | 8-0 |     	
+	*|valid|contextid|root |local_rank|children|lg_commsize|third|second|first|
+	
 
 /////////////////////////////////////////////////////////////////////////////////*/
 
-module reduce_instr(packetOut, rd_en, packetIn, clk, rst);
+module reduce_instr(packetOut, rd_en, packetIn, new_comm, clk, rst);
+
+//////////////////////////////////////////
+//current rank, root, and world size
+parameter cur_rank = 9'b0;
+parameter root = 9'b0;
+parameter rank_z = 3'b0;
+parameter rank_y = 3'b0;
+parameter rank_x = 3'b0;
+parameter root_z = 3'b0;
+parameter root_y = 3'b0;
+parameter root_x = 3'b0;
+
+parameter lg_numprocs = 3;
+parameter num_procs = 1 << lg_numprocs;
+
+///////////////////////////////////////////
+//packet structure
+parameter PayloadWidth=32;
+parameter opPos = PayloadWidth;
+parameter opWidth = 4;
+parameter AlgTypePos = opPos+opWidth;
+parameter AlgTypeWidth = 2;
+parameter TagPos=AlgTypePos+AlgTypeWidth;
+parameter TagWidth = 8;
+parameter ContextIdPos = TagPos+TagWidth;
+parameter ContextIdWidth = 8;
+parameter RankPos = ContextIdPos + ContextIdWidth;
+parameter RankWidth = 9;
+parameter Src_XPos = RankPos+RankWidth;
+parameter Src_XWidth = 3;
+parameter Src_YPos = Src_XPos+Src_XWidth;
+parameter Src_YWidth = 3;
+parameter Src_ZPos = Src_YPos+Src_YWidth;
+parameter Src_ZWidth = 3;
+parameter Dst_XPos = Src_ZPos+Src_ZWidth;
+parameter Dst_XWidth = 3;
+parameter Dst_YPos = Dst_XPos+Dst_XWidth;
+parameter Dst_YWidth = 3;
+parameter Dst_ZPos = Dst_YPos+Dst_YWidth;
+parameter Dst_ZWidth = 3;
+parameter SrcPos = Src_XPos;
+parameter SrcWidth = Src_XWidth+Src_YWidth+Src_ZWidth;
+parameter DstPos = Dst_XPos;
+parameter DstWidth = Dst_XWidth+Dst_YWidth+Dst_ZWidth;
+parameter ValidBitPos = Dst_ZPos+Dst_ZWidth;
+parameter FlitWidth = ValidBitPos + 1;
+
+
+/////////////////////////////////////////
+//children and countdown
+parameter ChildrenPos=ValidBitPos+1;
+parameter ChildrenWidth=lg_numprocs;
+parameter WaitPos = ChildrenPos+ChildrenWidth;
+parameter WaitWidth = 4;
+parameter ExtraWaitPos=WaitPos+WaitWidth;
+parameter LeafBitPos=ExtraWaitPos+1;
+
+ 
+//////////////////////////////////////////
+//reduce unit table and adder
+parameter ReductionTableWidth = LeafBitPos+1;
+parameter ReductionTableSize = 2;
+parameter AdderLatency = 14;
+parameter ReductionBitPos=opPos+opWidth-1;
+
+
+///////////////////////////////////////
+//fifo
+parameter fifo_lg_size = 12;
+parameter FifoSize = 1<<fifo_lg_size;
+
+/////////////////////////////////////
+//communicator table
+parameter CommTableWidth = (lg_numprocs+2)*DstWidth + lg_numprocs*2+2;
+parameter CommTableSize = 4;
+parameter lgCommSizePos = lg_numprocs*DstWidth;
+parameter CommChildrenPos = lgCommSizePos+lg_numprocs+1;
+parameter LocalRankPos = CommChildrenPos + lg_numprocs;
+parameter NewCommWidth = CommTableWidth+ContextIdWidth;
+
+////////////////////////////////////
+//algorithmic opcodes
+parameter Scan = 4'b0011;
+parameter AlltoAll = 4'b0100;
+parameter LargeBcast = 4'b0101;
+parameter MediumBcast = 4'b0110;
+parameter ShortBcast = 4'b0111;
+parameter Scatter = 4'b1000;
+parameter LargeAllGather = 4'b1001;
+parameter ShortAllGather = 4'b1010;
+parameter Gather = 4'b1011;
+parameter ShortReduce = 4'b1100;
+parameter LargeReduce = 4'b1101;
+parameter ShortAllReduce = 4'b1110;
+parameter LargeAllReduce = 4'b1111;
+
+////////////////////////////////////
 
 input [FlitWidth-1:0] packetIn;
+input [NewCommWidth-1:0] new_comm;
 input clk, rst;
 output rd_en;
 output [FlitWidth+ChildrenWidth-1:0] packetOut;
@@ -93,6 +197,7 @@ end*/
 //communicator table
 reg[CommTableWidth-1:0] comm_table[CommTableSize-1:0];
 reg [CommTableSize-1:0]i;
+wire [ContextIdWidth-1:0]new_comm_context = new_comm[NewCommWidth-2:NewCommWidth-1-ContextIdWidth];
 
 always @(posedge clk) begin
  if (rst) begin //if rst, set everything to 0
@@ -100,9 +205,12 @@ always @(posedge clk) begin
 	comm_table[i]<=0;
   end	
  end 
- else if (packetIn[opPos+opWidth-1:opPos] == Communicator)begin
-	comm_table[packetIn[ContextIdPos+ContextIdWidth-1:ContextIdPos]] <= packetIn[CommTableWidth-1:0];
- end 
+ 
+ else if(new_comm[NewCommWidth-1])begin
+		comm_table[new_comm_context][CommTableWidth-2:0] <= new_comm[CommTableWidth-2:0];	
+		comm_table[new_comm_context][CommTableWidth-1] <= 1'b1;
+ end
+ 
  comm_table[0] <= {1'b1, 9'b0, 9'b0, 3'b011, 4'b0011, 9'b01, 9'b10, 9'b100};
  //comm_table[0] <= {1'b1, 9'b0, 9'b000000100, 3'b010, 4'b0011, 9'b000000101, 9'b000000110, 9'b000};
  //comm_table[0] <= {1'b1, 9'b0, 9'b000000010, 3'b001, 4'b0011, 9'b000000011, 9'b0, 9'b000000110};
@@ -350,13 +458,6 @@ always @(posedge clk) begin
 	end
 	else if((meant_for_me)&&(t_op>4'b0)) begin
 		case(t_op)
-			Communicator:	begin
-									op <= 0;
-									algtype <= 0;
-									children <= 0;
-									rd_en_reg <= 1;
-									{dst1, dst2, dst3} <= 0;
-								end		
 			LargeBcast: 	begin
 									op <= (!home_halving)? LargeBcast : LargeAllGather;
 									algtype <= 0;
@@ -507,6 +608,7 @@ assign packetOut[RankPos+RankWidth-1:RankPos] = ((ok_meant_for_me)&&({dst3, dst2
 assign packetOut[SrcPos+SrcWidth-1:SrcPos] = ((ok_meant_for_me)&&({dst3, dst2, dst1}!={rank_z, rank_y, rank_x}))? {rank_z, rank_y, rank_x} : {src_z, src_y, src_x};
 assign packetOut[ValidBitPos] = valid;
 assign packetOut[ChildrenPos+ChildrenWidth-1:ChildrenPos] = children;
+
 
 endmodule
 
