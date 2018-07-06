31d044b63e61747b00bce43a1299c8be4ef5ab00
joshstern1 <jstern19@bu.edu>
2018-07-06 15:27:08 -0400

    Update large_buffer.v

--- Src/large_buffer.v
+++ Src/large_buffer.v
@@ -1,43 +1,21 @@
 `timescale 1ns / 1ns
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:30:51 06/29/2018 
-// Design Name: 
-// Module Name:    large_buffer 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module large_buffer(clk, rst, in, produce, consume, full, empty, out, usedw);
-    
-	 parameter FLIT_SIZE = 82;
-	 parameter buffer_width = 82;
-	 parameter buffer_depth = 5;
-	 parameter cur_x = 0;
-	 parameter cur_y = 0;
-	 parameter cur_z = 0;
-	 
-	 input clk;
-    input rst;
-    input [buffer_width-1:0] in;
-    input produce;
-    input consume;
-    output full;
-    output empty;
-    output reg [buffer_width-1:0] out;
-    output [buffer_width-1:0] usedw;
-
-    
+module large_buffer
+#(
+    parameter buffer_depth=8,
+    parameter buffer_width=64
+)(
+	 input clk,
+    input rst,
+    input [buffer_width-1:0] in,
+    input produce,
+    input consume,
+    output full,
+    output empty,
+    output reg [buffer_width-1:0] out,
+    output [buffer_width-1:0] usedw
+);
+     
     reg [buffer_depth/2:0] i;
 
     wire[buffer_depth-1:0] head_next;
@@ -88,17 +66,6 @@ module large_buffer(clk, rst, in, produce, consume, full, empty, out, usedw);
         else begin
             if(consume && ~empty)
                 head <= head_next;
-        end
-    end
-
-    reg [buffer_width - 1 : 0] usedw_reg;
-    assign usedw = usedw_reg;
-    always@(*) begin
-        if(tail >= head) begin
-            usedw_reg = tail - head;
-        end
-        else begin
-            usedw_reg = buffer_depth - head + tail;
         end
     end
 
