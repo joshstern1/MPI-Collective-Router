bb55ab8bddb6a0b71c6253b3e309e5109124588a
joshstern1 <jstern19@bu.edu>
2018-07-09 13:52:11 -0400

    Update network.v

--- Src/network.v
+++ Src/network.v
@@ -3,18 +3,26 @@
     module network(
     input clk,
     input rst,
-	 input [FLIT_SIZE : 0] in_xpos_ser_0_0_0,
-	 input [FLIT_SIZE : 0] in_ypos_ser_0_0_0,
-	 output [FLIT_SIZE : 0]out_xpos_ser_0_0_0,
-	 output [FLIT_SIZE : 0]out_ypos_ser_0_0_0
+	 input [FLIT_SIZE : 0]in_xpos_inject_0_0_0,
+	 input [FLIT_SIZE : 0]in_ypos_inject_0_0_0/*,
+	 input [FLIT_SIZE : 0]in_zpos_inject_0_0_0,
+	 input [FLIT_SIZE : 0]in_xneg_inject_0_0_0,
+	 input [FLIT_SIZE : 0]in_yneg_inject_0_0_0,
+	 input [FLIT_SIZE : 0]in_zneg_inject_0_0_0*/
+	 /*input [FLIT_SIZE : 0]in_xpos_inject_0_0_1,
+	 input [FLIT_SIZE : 0]in_ypos_inject_0_0_1,
+	 input [FLIT_SIZE : 0]in_zpos_inject_0_0_1,
+	 input [FLIT_SIZE : 0]in_xneg_inject_0_0_1,
+	 input [FLIT_SIZE : 0]in_yneg_inject_0_0_1,
+	 input [FLIT_SIZE : 0]in_zneg_inject_0_0_1*/
 );
 
 	parameter FLIT_SIZE = 82;
 
 
-	//wire[FLIT_SIZE : 0] in_xpos_ser_0_0_0, out_xpos_ser_0_0_0;
+	wire[FLIT_SIZE : 0] in_xpos_ser_0_0_0, out_xpos_ser_0_0_0;
 	//wire[FLIT_SIZE : 0] in_xneg_ser_0_0_0, out_xneg_ser_0_0_0;
-	//wire[FLIT_SIZE : 0] in_ypos_ser_0_0_0, out_ypos_ser_0_0_0;
+	wire[FLIT_SIZE : 0] in_ypos_ser_0_0_0, out_ypos_ser_0_0_0;
 	//wire[FLIT_SIZE : 0] in_yneg_ser_0_0_0, out_yneg_ser_0_0_0;
 	//wire[FLIT_SIZE : 0] in_zpos_ser_0_0_0, out_zpos_ser_0_0_0;
 	//wire[FLIT_SIZE : 0] in_zneg_ser_0_0_0, out_zneg_ser_0_0_0;
@@ -132,6 +140,12 @@
         )n_0_0_0(
         .clk(clk),
         .rst(rst),
+		  .inject_xpos(in_xpos_inject_0_0_0),
+		  .inject_ypos(in_ypos_inject_0_0_0),
+		  //.inject_zpos(in_zpos_inject_0_0_0),
+		  //.inject_xneg(in_xneg_inject_0_0_0),
+		  //.inject_yneg(in_yneg_inject_0_0_0),
+		  //.inject_zneg(in_zneg_inject_0_0_0),
         .in_xpos_ser(in_xpos_ser_0_0_0),
         .out_xpos_ser(out_xpos_ser_0_0_0),
         //.in_xneg_ser(in_xneg_ser_0_0_0),
@@ -145,7 +159,7 @@
         .in_zneg_ser(in_zneg_ser_0_0_0),
         .out_zneg_ser(out_zneg_ser_0_0_0)*/
       );
-    node#(
+    /*node#(
         .cur_x(0),
         .cur_y(0),
         .cur_z(1)
@@ -154,17 +168,17 @@
         .rst(rst),
         .in_xpos_ser(in_xpos_ser_0_0_1),
         .out_xpos_ser(out_xpos_ser_0_0_1),
-        //.in_xneg_ser(in_xneg_ser_0_0_1),
-       // .out_xneg_ser(out_xneg_ser_0_0_1),
+        .in_xneg_ser(in_xneg_ser_0_0_1),
+        .out_xneg_ser(out_xneg_ser_0_0_1),
         .in_ypos_ser(in_ypos_ser_0_0_1),
-        .out_ypos_ser(out_ypos_ser_0_0_1)/*,
+        .out_ypos_ser(out_ypos_ser_0_0_1),
         .in_yneg_ser(in_yneg_ser_0_0_1),
         .out_yneg_ser(out_yneg_ser_0_0_1),
         .in_zpos_ser(in_zpos_ser_0_0_1),
         .out_zpos_ser(out_zpos_ser_0_0_1),
         .in_zneg_ser(in_zneg_ser_0_0_1),
-        .out_zneg_ser(out_zneg_ser_0_0_1)*/
-      );
+        .out_zneg_ser(out_zneg_ser_0_0_1)
+      );*/
     /*node#(
         .cur_x(0),
         .cur_y(1),
