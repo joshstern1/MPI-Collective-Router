195fcc797d40479cf137cab7f41524852e3639d4
joshstern1 <jstern19@bu.edu>
2018-07-19 09:57:00 -0400

    Update network_16.v

--- Src/network_16.v
+++ Src/network_16.v
@@ -152,7 +152,7 @@
 );
 
 	localparam num_procs = 1 << lg_numprocs;
-	localparam lg_numprocs = 3;
+	localparam lg_numprocs = 4;
 
 	//packet structure
 	localparam PayloadWidth=32;
@@ -312,21 +312,38 @@
 
 
 	assign in_xpos_ser_0_0_0=out_xneg_ser_0_0_1;
-	assign in_xneg_ser_0_0_0=out_xpos_ser_0_0_1;
-	assign in_xpos_ser_0_0_1=out_xneg_ser_0_0_0;
+	assign in_xneg_ser_0_0_0=out_xpos_ser_0_0_3;
+	assign in_xpos_ser_0_0_1=out_xneg_ser_0_0_2;
 	assign in_xneg_ser_0_0_1=out_xpos_ser_0_0_0;
 	assign in_xpos_ser_0_1_0=out_xneg_ser_0_1_1;
-	assign in_xneg_ser_0_1_0=out_xpos_ser_0_1_1;
-	assign in_xpos_ser_0_1_1=out_xneg_ser_0_1_0;
+	assign in_xneg_ser_0_1_0=out_xpos_ser_0_1_3;
+	assign in_xpos_ser_0_1_1=out_xneg_ser_0_1_2;
 	assign in_xneg_ser_0_1_1=out_xpos_ser_0_1_0;
 	assign in_xpos_ser_1_0_0=out_xneg_ser_1_0_1;
-	assign in_xneg_ser_1_0_0=out_xpos_ser_1_0_1;
-	assign in_xpos_ser_1_0_1=out_xneg_ser_1_0_0;
+	assign in_xneg_ser_1_0_0=out_xpos_ser_1_0_3;
+	assign in_xpos_ser_1_0_1=out_xneg_ser_1_0_2;
 	assign in_xneg_ser_1_0_1=out_xpos_ser_1_0_0;
 	assign in_xpos_ser_1_1_0=out_xneg_ser_1_1_1;
-	assign in_xneg_ser_1_1_0=out_xpos_ser_1_1_1;
-	assign in_xpos_ser_1_1_1=out_xneg_ser_1_1_0;
+	assign in_xneg_ser_1_1_0=out_xpos_ser_1_1_3;
+	assign in_xpos_ser_1_1_1=out_xneg_ser_1_1_2;
 	assign in_xneg_ser_1_1_1=out_xpos_ser_1_1_0;
+	
+	assign in_xpos_ser_0_0_3=out_xneg_ser_0_0_0;
+	assign in_xneg_ser_0_0_3=out_xpos_ser_0_0_2;
+	assign in_xpos_ser_0_0_2=out_xneg_ser_0_0_3;
+	assign in_xneg_ser_0_0_2=out_xpos_ser_0_0_1;
+	assign in_xpos_ser_0_1_3=out_xneg_ser_0_1_0;
+	assign in_xneg_ser_0_1_3=out_xpos_ser_0_1_2;
+	assign in_xpos_ser_0_1_2=out_xneg_ser_0_1_3;
+	assign in_xneg_ser_0_1_2=out_xpos_ser_0_1_1;
+	assign in_xpos_ser_1_0_3=out_xneg_ser_1_0_0;
+	assign in_xneg_ser_1_0_3=out_xpos_ser_1_0_2;
+	assign in_xpos_ser_1_0_2=out_xneg_ser_1_0_3;
+	assign in_xneg_ser_1_0_2=out_xpos_ser_1_0_1;
+	assign in_xpos_ser_1_1_3=out_xneg_ser_1_1_0;
+	assign in_xneg_ser_1_1_3=out_xpos_ser_1_1_2;
+	assign in_xpos_ser_1_1_2=out_xneg_ser_1_1_3;
+	assign in_xneg_ser_1_1_2=out_xpos_ser_1_1_1;
 
 	
 	assign in_ypos_ser_0_0_0=out_yneg_ser_0_1_0;
@@ -346,6 +363,23 @@
 	assign in_ypos_ser_1_1_1=out_yneg_ser_1_0_1;
 	assign in_yneg_ser_1_1_1=out_ypos_ser_1_0_1;
 	
+	assign in_ypos_ser_0_0_3=out_yneg_ser_0_1_3;
+	assign in_yneg_ser_0_0_3=out_ypos_ser_0_1_3;
+	assign in_ypos_ser_0_0_2=out_yneg_ser_0_1_2;
+	assign in_yneg_ser_0_0_2=out_ypos_ser_0_1_2;
+	assign in_ypos_ser_0_1_3=out_yneg_ser_0_0_3;
+	assign in_yneg_ser_0_1_3=out_ypos_ser_0_0_3;
+	assign in_ypos_ser_0_1_2=out_yneg_ser_0_0_2;
+	assign in_yneg_ser_0_1_2=out_ypos_ser_0_0_2;
+	assign in_ypos_ser_1_0_3=out_yneg_ser_1_1_3;
+	assign in_yneg_ser_1_0_3=out_ypos_ser_1_1_3;
+	assign in_ypos_ser_1_0_2=out_yneg_ser_1_1_2;
+	assign in_yneg_ser_1_0_2=out_ypos_ser_1_1_2;
+	assign in_ypos_ser_1_1_3=out_yneg_ser_1_0_3;
+	assign in_yneg_ser_1_1_3=out_ypos_ser_1_0_3;
+	assign in_ypos_ser_1_1_2=out_yneg_ser_1_0_2;
+	assign in_yneg_ser_1_1_2=out_ypos_ser_1_0_2;
+	
 
 	assign in_zpos_ser_0_0_0=out_zneg_ser_1_0_0;
 	assign in_zneg_ser_0_0_0=out_zpos_ser_1_0_0;
@@ -363,6 +397,23 @@
 	assign in_zneg_ser_1_1_0=out_zpos_ser_0_1_0;
 	assign in_zpos_ser_1_1_1=out_zneg_ser_0_1_1;
 	assign in_zneg_ser_1_1_1=out_zpos_ser_0_1_1;
+	
+	assign in_zpos_ser_0_0_3=out_zneg_ser_1_0_3;
+	assign in_zneg_ser_0_0_3=out_zpos_ser_1_0_3;
+	assign in_zpos_ser_0_0_2=out_zneg_ser_1_0_2;
+	assign in_zneg_ser_0_0_2=out_zpos_ser_1_0_2;
+	assign in_zpos_ser_0_1_3=out_zneg_ser_1_1_3;
+	assign in_zneg_ser_0_1_3=out_zpos_ser_1_1_3;
+	assign in_zpos_ser_0_1_2=out_zneg_ser_1_1_2;
+	assign in_zneg_ser_0_1_2=out_zpos_ser_1_1_2;
+	assign in_zpos_ser_1_0_3=out_zneg_ser_0_0_3;
+	assign in_zneg_ser_1_0_3=out_zpos_ser_0_0_3;
+	assign in_zpos_ser_1_0_2=out_zneg_ser_0_0_2;
+	assign in_zneg_ser_1_0_2=out_zpos_ser_0_0_2;
+	assign in_zpos_ser_1_1_3=out_zneg_ser_0_1_3;
+	assign in_zneg_ser_1_1_3=out_zpos_ser_0_1_3;
+	assign in_zpos_ser_1_1_2=out_zneg_ser_0_1_2;
+	assign in_zneg_ser_1_1_2=out_zpos_ser_0_1_2;
 
 	
     node_16#(
@@ -613,4 +664,253 @@
 		  .reduce_me(reduce_me_1_1_1),
 		  .newcomm(newcomm_1_1_1)
       );
+		
+	node_16#(
+        .cur_z(0),
+        .cur_y(0),
+        .cur_x(3),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_0_0_3(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_0_0_3),
+		  .inject_ypos(in_ypos_inject_0_0_3),
+		  .inject_zpos(in_zpos_inject_0_0_3),
+		  .inject_xneg(in_xneg_inject_0_0_3),
+		  .inject_yneg(in_yneg_inject_0_0_3),
+		  .inject_zneg(in_zneg_inject_0_0_3),
+        .in_xpos_ser(in_xpos_ser_0_0_3),
+        .out_xpos_ser(out_xpos_ser_0_0_3),
+        .in_xneg_ser(in_xneg_ser_0_0_3),
+        .out_xneg_ser(out_xneg_ser_0_0_3),
+        .in_ypos_ser(in_ypos_ser_0_0_3),
+        .out_ypos_ser(out_ypos_ser_0_0_3),
+        .in_yneg_ser(in_yneg_ser_0_0_3),
+        .out_yneg_ser(out_yneg_ser_0_0_3),
+        .in_zpos_ser(in_zpos_ser_0_0_3),
+        .out_zpos_ser(out_zpos_ser_0_0_3),
+        .in_zneg_ser(in_zneg_ser_0_0_3),
+        .out_zneg_ser(out_zneg_ser_0_0_3),
+		  .reduce_me(reduce_me_0_0_3),
+		  .newcomm(newcomm_0_0_3)
+      );
+		
+	node_16#(
+        .cur_z(0),
+        .cur_y(0),
+        .cur_x(2),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_0_0_2(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_0_0_2),
+		  .inject_ypos(in_ypos_inject_0_0_2),
+		  .inject_zpos(in_zpos_inject_0_0_2),
+		  .inject_xneg(in_xneg_inject_0_0_2),
+		  .inject_yneg(in_yneg_inject_0_0_2),
+		  .inject_zneg(in_zneg_inject_0_0_2),
+        .in_xpos_ser(in_xpos_ser_0_0_2),
+        .out_xpos_ser(out_xpos_ser_0_0_2),
+        .in_xneg_ser(in_xneg_ser_0_0_2),
+        .out_xneg_ser(out_xneg_ser_0_0_2),
+        .in_ypos_ser(in_ypos_ser_0_0_2),
+        .out_ypos_ser(out_ypos_ser_0_0_2),
+        .in_yneg_ser(in_yneg_ser_0_0_2),
+        .out_yneg_ser(out_yneg_ser_0_0_2),
+        .in_zpos_ser(in_zpos_ser_0_0_2),
+        .out_zpos_ser(out_zpos_ser_0_0_2),
+        .in_zneg_ser(in_zneg_ser_0_0_2),
+        .out_zneg_ser(out_zneg_ser_0_0_2),
+		  .reduce_me(reduce_me_0_0_2),
+		  .newcomm(newcomm_0_0_2)
+      );
+		
+	node_16#(
+        .cur_z(0),
+        .cur_y(1),
+        .cur_x(3),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_0_1_3(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_0_1_3),
+		  .inject_ypos(in_ypos_inject_0_1_3),
+		  .inject_zpos(in_zpos_inject_0_1_3),
+		  .inject_xneg(in_xneg_inject_0_1_3),
+		  .inject_yneg(in_yneg_inject_0_1_3),
+		  .inject_zneg(in_zneg_inject_0_1_3),
+        .in_xpos_ser(in_xpos_ser_0_1_3),
+        .out_xpos_ser(out_xpos_ser_0_1_3),
+        .in_xneg_ser(in_xneg_ser_0_1_3),
+        .out_xneg_ser(out_xneg_ser_0_1_3),
+        .in_ypos_ser(in_ypos_ser_0_1_3),
+        .out_ypos_ser(out_ypos_ser_0_1_3),
+        .in_yneg_ser(in_yneg_ser_0_1_3),
+        .out_yneg_ser(out_yneg_ser_0_1_3),
+        .in_zpos_ser(in_zpos_ser_0_1_3),
+        .out_zpos_ser(out_zpos_ser_0_1_3),
+        .in_zneg_ser(in_zneg_ser_0_1_3),
+        .out_zneg_ser(out_zneg_ser_0_1_3),
+		  .reduce_me(reduce_me_0_1_3),
+		  .newcomm(newcomm_0_1_3)
+      );
+		
+	node_16#(
+        .cur_z(0),
+        .cur_y(1),
+        .cur_x(2),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_0_1_2(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_0_1_2),
+		  .inject_ypos(in_ypos_inject_0_1_2),
+		  .inject_zpos(in_zpos_inject_0_1_2),
+		  .inject_xneg(in_xneg_inject_0_1_2),
+		  .inject_yneg(in_yneg_inject_0_1_2),
+		  .inject_zneg(in_zneg_inject_0_1_2),
+        .in_xpos_ser(in_xpos_ser_0_1_2),
+        .out_xpos_ser(out_xpos_ser_0_1_2),
+        .in_xneg_ser(in_xneg_ser_0_1_2),
+        .out_xneg_ser(out_xneg_ser_0_1_2),
+        .in_ypos_ser(in_ypos_ser_0_1_2),
+        .out_ypos_ser(out_ypos_ser_0_1_2),
+        .in_yneg_ser(in_yneg_ser_0_1_2),
+        .out_yneg_ser(out_yneg_ser_0_1_2),
+        .in_zpos_ser(in_zpos_ser_0_1_2),
+        .out_zpos_ser(out_zpos_ser_0_1_2),
+        .in_zneg_ser(in_zneg_ser_0_1_2),
+        .out_zneg_ser(out_zneg_ser_0_1_2),
+		  .reduce_me(reduce_me_0_1_2),
+		  .newcomm(newcomm_0_1_2)
+      );
+
+	node_16#(
+        .cur_z(1),
+        .cur_y(0),
+        .cur_x(3),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_1_0_3(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_1_0_3),
+		  .inject_ypos(in_ypos_inject_1_0_3),
+		  .inject_zpos(in_zpos_inject_1_0_3),
+		  .inject_xneg(in_xneg_inject_1_0_3),
+		  .inject_yneg(in_yneg_inject_1_0_3),
+		  .inject_zneg(in_zneg_inject_1_0_3),
+        .in_xpos_ser(in_xpos_ser_1_0_3),
+        .out_xpos_ser(out_xpos_ser_1_0_3),
+        .in_xneg_ser(in_xneg_ser_1_0_3),
+        .out_xneg_ser(out_xneg_ser_1_0_3),
+        .in_ypos_ser(in_ypos_ser_1_0_3),
+        .out_ypos_ser(out_ypos_ser_1_0_3),
+        .in_yneg_ser(in_yneg_ser_1_0_3),
+        .out_yneg_ser(out_yneg_ser_1_0_3),
+        .in_zpos_ser(in_zpos_ser_1_0_3),
+        .out_zpos_ser(out_zpos_ser_1_0_3),
+        .in_zneg_ser(in_zneg_ser_1_0_3),
+        .out_zneg_ser(out_zneg_ser_1_0_3),
+		  .reduce_me(reduce_me_1_0_3),
+		  .newcomm(newcomm_1_0_3)
+      );
+		
+	node_16#(
+        .cur_z(1),
+        .cur_y(0),
+        .cur_x(2),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_1_0_2(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_1_0_2),
+		  .inject_ypos(in_ypos_inject_1_0_2),
+		  .inject_zpos(in_zpos_inject_1_0_2),
+		  .inject_xneg(in_xneg_inject_1_0_2),
+		  .inject_yneg(in_yneg_inject_1_0_2),
+		  .inject_zneg(in_zneg_inject_1_0_2),
+        .in_xpos_ser(in_xpos_ser_1_0_2),
+        .out_xpos_ser(out_xpos_ser_1_0_2),
+        .in_xneg_ser(in_xneg_ser_1_0_2),
+        .out_xneg_ser(out_xneg_ser_1_0_2),
+        .in_ypos_ser(in_ypos_ser_1_0_2),
+        .out_ypos_ser(out_ypos_ser_1_0_2),
+        .in_yneg_ser(in_yneg_ser_1_0_2),
+        .out_yneg_ser(out_yneg_ser_1_0_2),
+        .in_zpos_ser(in_zpos_ser_1_0_2),
+        .out_zpos_ser(out_zpos_ser_1_0_2),
+        .in_zneg_ser(in_zneg_ser_1_0_2),
+        .out_zneg_ser(out_zneg_ser_1_0_2),
+		  .reduce_me(reduce_me_1_0_2),
+		  .newcomm(newcomm_1_0_2)
+      );
+		
+	node_16#(
+        .cur_z(1),
+        .cur_y(1),
+        .cur_x(3),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_1_1_3(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_1_1_3),
+		  .inject_ypos(in_ypos_inject_1_1_3),
+		  .inject_zpos(in_zpos_inject_1_1_3),
+		  .inject_xneg(in_xneg_inject_1_1_3),
+		  .inject_yneg(in_yneg_inject_1_1_3),
+		  .inject_zneg(in_zneg_inject_1_1_3),
+        .in_xpos_ser(in_xpos_ser_1_1_3),
+        .out_xpos_ser(out_xpos_ser_1_1_3),
+        .in_xneg_ser(in_xneg_ser_1_1_3),
+        .out_xneg_ser(out_xneg_ser_1_1_3),
+        .in_ypos_ser(in_ypos_ser_1_1_3),
+        .out_ypos_ser(out_ypos_ser_1_1_3),
+        .in_yneg_ser(in_yneg_ser_1_1_3),
+        .out_yneg_ser(out_yneg_ser_1_1_3),
+        .in_zpos_ser(in_zpos_ser_1_1_3),
+        .out_zpos_ser(out_zpos_ser_1_1_3),
+        .in_zneg_ser(in_zneg_ser_1_1_3),
+        .out_zneg_ser(out_zneg_ser_1_1_3),
+		  .reduce_me(reduce_me_1_1_3),
+		  .newcomm(newcomm_1_1_3)
+      );
+		
+	node_16#(
+        .cur_z(1),
+        .cur_y(1),
+        .cur_x(2),
+		  .lg_numprocs(lg_numprocs),
+		  .PayloadWidth(PayloadWidth)
+        )n_1_1_2(
+        .clk(clk),
+        .rst(rst),
+		  .inject_xpos(in_xpos_inject_1_1_2),
+		  .inject_ypos(in_ypos_inject_1_1_2),
+		  .inject_zpos(in_zpos_inject_1_1_2),
+		  .inject_xneg(in_xneg_inject_1_1_2),
+		  .inject_yneg(in_yneg_inject_1_1_2),
+		  .inject_zneg(in_zneg_inject_1_1_2),
+        .in_xpos_ser(in_xpos_ser_1_1_2),
+        .out_xpos_ser(out_xpos_ser_1_1_2),
+        .in_xneg_ser(in_xneg_ser_1_1_2),
+        .out_xneg_ser(out_xneg_ser_1_1_2),
+        .in_ypos_ser(in_ypos_ser_1_1_2),
+        .out_ypos_ser(out_ypos_ser_1_1_2),
+        .in_yneg_ser(in_yneg_ser_1_1_2),
+        .out_yneg_ser(out_yneg_ser_1_1_2),
+        .in_zpos_ser(in_zpos_ser_1_1_2),
+        .out_zpos_ser(out_zpos_ser_1_1_2),
+        .in_zneg_ser(in_zneg_ser_1_1_2),
+        .out_zneg_ser(out_zneg_ser_1_1_2),
+		  .reduce_me(reduce_me_1_1_2),
+		  .newcomm(newcomm_1_1_2)
+      );
+		
 endmodule
