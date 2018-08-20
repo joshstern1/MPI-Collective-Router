73e2f41069541a0a04573eecc83833a6a60912fb
joshstern1 <jstern19@bu.edu>
2018-08-20 19:49:35 -0400

    Update switch.v

--- Src/switch.v
+++ Src/switch.v
@@ -41,12 +41,6 @@ module switch#(
     wire [M_IN - 1 : 0] yneg_in_valid;
     wire [M_IN - 1 : 0] zneg_in_valid;
 
-    wire [M_IN - 1 : 0] xpos_in_avail;
-    wire [M_IN - 1 : 0] ypos_in_avail;
-    wire [M_IN - 1 : 0] zpos_in_avail = 0;
-    wire [M_IN - 1 : 0] xneg_in_avail = 0;
-    wire [M_IN - 1 : 0] yneg_in_avail = 0;
-    wire [M_IN - 1 : 0] zneg_in_avail = 0;
 
     genvar i;
     generate 
@@ -164,80 +158,194 @@ module switch#(
 	 reg [ROUTE_LEN - 1 : 0] in_yneg_selector;
     reg [ROUTE_LEN - 1 : 0] in_zneg_selector;
 	 
+	 reg new_out_avail_xpos;
+    reg new_out_avail_ypos;
+	 reg new_out_avail_zpos;
+	 reg new_out_avail_xneg;
+	 reg new_out_avail_yneg;
+	 reg new_out_avail_zneg;
+	 
 	 
 	 always @(*)begin
 		case(route_in[ROUTE_LEN - 1 : 0])
-			DIR_XPOS:	in_xpos_selector = out_xpos_selector;							
-			DIR_YPOS:	in_xpos_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_xpos_selector = out_zpos_selector;
-			DIR_XNEG:	in_xpos_selector = out_xneg_selector;		
-			DIR_YNEG:	in_xpos_selector = out_yneg_selector;
-			DIR_ZNEG:	in_xpos_selector = out_zneg_selector;		
+			DIR_XPOS:	begin
+							in_xpos_selector = out_xpos_selector;
+							new_out_avail_xpos = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_xpos_selector = out_ypos_selector;	
+							new_out_avail_xpos = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_xpos_selector = out_zpos_selector;
+							new_out_avail_xpos = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_xpos_selector = out_xneg_selector;	
+							new_out_avail_xpos = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_xpos_selector = out_yneg_selector;
+							new_out_avail_xpos = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_xpos_selector = out_zneg_selector;	
+							new_out_avail_xpos = out_avail[5];
+							end
 		endcase
 	 end
 	 
 	 always @(*)begin
 		case(route_in[2*ROUTE_LEN - 1 : ROUTE_LEN])
-			DIR_XPOS:	in_ypos_selector = out_xpos_selector;							
-			DIR_YPOS:	in_ypos_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_ypos_selector = out_zpos_selector;
-			DIR_XNEG:	in_ypos_selector = out_xneg_selector;		
-			DIR_YNEG:	in_ypos_selector = out_yneg_selector;
-			DIR_ZNEG:	in_ypos_selector = out_zneg_selector;									
+			DIR_XPOS:	begin
+							in_ypos_selector = out_xpos_selector;
+							new_out_avail_ypos = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_ypos_selector = out_ypos_selector;	
+							new_out_avail_ypos = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_ypos_selector = out_zpos_selector;
+							new_out_avail_ypos = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_ypos_selector = out_xneg_selector;	
+							new_out_avail_ypos = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_ypos_selector = out_yneg_selector;
+							new_out_avail_ypos = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_ypos_selector = out_zneg_selector;	
+							new_out_avail_ypos = out_avail[5];
+							end									
 		endcase
 	 end
 	 
 	 always @(*)begin
 		case(route_in[3*ROUTE_LEN - 1 : 2*ROUTE_LEN])
-			DIR_XPOS:	in_zpos_selector = out_xpos_selector;							
-			DIR_YPOS:	in_zpos_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_zpos_selector = out_zpos_selector;
-			DIR_XNEG:	in_zpos_selector = out_xneg_selector;		
-			DIR_YNEG:	in_zpos_selector = out_yneg_selector;
-			DIR_ZNEG:	in_zpos_selector = out_zneg_selector;									
+			DIR_XPOS:	begin
+							in_zpos_selector = out_xpos_selector;
+							new_out_avail_zpos = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_zpos_selector = out_ypos_selector;	
+							new_out_avail_zpos = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_zpos_selector = out_zpos_selector;
+							new_out_avail_zpos = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_zpos_selector = out_xneg_selector;	
+							new_out_avail_zpos = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_zpos_selector = out_yneg_selector;
+							new_out_avail_zpos = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_zpos_selector = out_zneg_selector;	
+							new_out_avail_zpos = out_avail[5];
+							end								
 		endcase
 	 end
 	 
 	 always @(*)begin
 		case(route_in[4*ROUTE_LEN - 1 : 3*ROUTE_LEN])
-			DIR_XPOS:	in_xneg_selector = out_xpos_selector;							
-			DIR_YPOS:	in_xneg_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_xneg_selector = out_zpos_selector;
-			DIR_XNEG:	in_xneg_selector = out_xneg_selector;		
-			DIR_YNEG:	in_xneg_selector = out_yneg_selector;
-			DIR_ZNEG:	in_xneg_selector = out_zneg_selector;									
+			DIR_XPOS:	begin
+							in_xneg_selector = out_xpos_selector;
+							new_out_avail_xneg = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_xneg_selector = out_ypos_selector;	
+							new_out_avail_xneg = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_xneg_selector = out_zpos_selector;
+							new_out_avail_xneg = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_xneg_selector = out_xneg_selector;	
+							new_out_avail_xneg = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_xneg_selector = out_yneg_selector;
+							new_out_avail_xneg = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_xneg_selector = out_zneg_selector;	
+							new_out_avail_xneg = out_avail[5];
+							end								
 		endcase
 	 end
 	 
 	 always @(*)begin
 		case(route_in[5*ROUTE_LEN - 1 : 4*ROUTE_LEN])
-			DIR_XPOS:	in_yneg_selector = out_xpos_selector;							
-			DIR_YPOS:	in_yneg_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_yneg_selector = out_zpos_selector;
-			DIR_XNEG:	in_yneg_selector = out_xneg_selector;		
-			DIR_YNEG:	in_yneg_selector = out_yneg_selector;
-			DIR_ZNEG:	in_yneg_selector = out_zneg_selector;									
+			DIR_XPOS:	begin
+							in_yneg_selector = out_xpos_selector;
+							new_out_avail_yneg = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_yneg_selector = out_ypos_selector;	
+							new_out_avail_yneg = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_yneg_selector = out_zpos_selector;
+							new_out_avail_yneg = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_yneg_selector = out_xneg_selector;	
+							new_out_avail_yneg = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_yneg_selector = out_yneg_selector;
+							new_out_avail_yneg = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_yneg_selector = out_zneg_selector;	
+							new_out_avail_yneg = out_avail[5];
+							end									
 		endcase
 	 end
 	 
 	 always @(*)begin
 		case(route_in[6*ROUTE_LEN - 1 : 5*ROUTE_LEN])
-			DIR_XPOS:	in_zneg_selector = out_xpos_selector;							
-			DIR_YPOS:	in_zneg_selector = out_ypos_selector;		
-			DIR_ZPOS:	in_zneg_selector = out_zpos_selector;
-			DIR_XNEG:	in_zneg_selector = out_xneg_selector;		
-			DIR_YNEG:	in_zneg_selector = out_yneg_selector;
-			DIR_ZNEG:	in_zneg_selector = out_zneg_selector;									
+			DIR_XPOS:	begin
+							in_zneg_selector = out_xpos_selector;
+							new_out_avail_zneg = out_avail[0];
+							end
+			DIR_YPOS:	begin
+							in_zneg_selector = out_ypos_selector;	
+							new_out_avail_zneg = out_avail[1];	
+							end
+			DIR_ZPOS:	begin
+							in_zneg_selector = out_zpos_selector;
+							new_out_avail_zneg = out_avail[2];
+							end
+			DIR_XNEG:	begin
+							in_zneg_selector = out_xneg_selector;	
+							new_out_avail_zneg = out_avail[3];		
+							end
+			DIR_YNEG:	begin
+							in_zneg_selector = out_yneg_selector;
+							new_out_avail_zneg = out_avail[4];
+							end
+			DIR_ZNEG:	begin
+							in_zneg_selector = out_zneg_selector;	
+							new_out_avail_zneg = out_avail[5];
+							end									
 		endcase
 	 end
 	 
 	 	 
-	 assign in_avail[0] = !(((in_xpos_selector!=0) && (in_valid[0]))||(out_avail[0]));
-	 assign in_avail[1] = !(((in_ypos_selector!=1) && (in_valid[1]))||(out_avail[1]));
-	 assign in_avail[2] = !(((in_zpos_selector!=2) && (in_valid[2]))||(out_avail[2]));
-	 assign in_avail[3] = !(((in_xneg_selector!=3) && (in_valid[3]))||(out_avail[3]));
-	 assign in_avail[4] = !(((in_yneg_selector!=4) && (in_valid[4]))||(out_avail[4]));
-	 assign in_avail[5] = !(((in_zneg_selector!=5) && (in_valid[5]))||(out_avail[5]));
+	 assign in_avail[0] = !(((in_xpos_selector!=0) && (in_valid[0]))||(new_out_avail_xpos));
+	 assign in_avail[1] = !(((in_ypos_selector!=1) && (in_valid[1]))||(new_out_avail_ypos));
+	 assign in_avail[2] = !(((in_zpos_selector!=2) && (in_valid[2]))||(new_out_avail_zpos));
+	 assign in_avail[3] = !(((in_xneg_selector!=3) && (in_valid[3]))||(new_out_avail_xneg));
+	 assign in_avail[4] = !(((in_yneg_selector!=4) && (in_valid[4]))||(new_out_avail_yneg));
+	 assign in_avail[5] = !(((in_zneg_selector!=5) && (in_valid[5]))||(new_out_avail_zneg));
    
 endmodule
- 
